2c1007904aed2469bd220ad4cf4950609cedfdfd
kee111 <eqrlgrey@gmail.com>
2021-06-25 15:43:34 +0900

    説明文の修正

--- Work3/Work3.java
+++ Work3/Work3.java
@@ -7,8 +7,8 @@ public class Work3 {
         Random rand = new Random();
         int num = rand.nextInt(100) + 1;
 
-        // javaでのint型の計算は、int型で帰ってくるので
-        // 10で割ったときの小数点以下は切り捨てられ、二桁目の数字が求められる
+        // javaでのint型の計算は、int型で帰ってくる
+        // よって10で割ったときの小数点以下は切り捨てられ、二桁目の数字が求められる
         // しかしこの計算方法が通用するのは100以下の整数だけになる
         System.out.println("作成された乱数:" + num);
         System.out.println("二桁目の数字は" + (num / 10));
