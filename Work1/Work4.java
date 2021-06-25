f7ff534f64ab510b7bae12a4df11dc341136f3e8
kee111 <eqrlgrey@gmail.com>
2021-06-25 15:46:12 +0900

    説明文の追加

--- Work1/Work4.java
+++ Work1/Work4.java
@@ -3,6 +3,8 @@ package Work1;
 
 public class Work4 {
     public static void main(String[] args) {
+        // プログラム引数で受け取った要素はString型になる
+        // 計算に使えるようにint型に変換している
         int args1 = Integer.parseInt(args[0]);
         int args2 = Integer.parseInt(args[1]);
         
